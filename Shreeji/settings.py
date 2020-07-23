f9c4013b3ac8a673aa4b1fc7ecf6085748f1b50d
Bhanderi Meet <55754435+meetbhanderi@users.noreply.github.com>
2020-07-23 21:50:30 +0530

    Static Chnages

--- Shreeji/settings.py
+++ Shreeji/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT = STATIC_DIR
 
 MESSAGE_TAGS = {
     messages.ERROR : 'danger'
