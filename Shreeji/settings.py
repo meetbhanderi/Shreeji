1392b4cad7f3d0b8c2b0d7d2d59a5609669b3c72
Bhanderi Meet <55754435+meetbhanderi@users.noreply.github.com>
2020-07-25 17:29:42 +0530

    files added

--- Shreeji/settings.py
+++ Shreeji/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_ROOT = STATIC_DIR
+# STATIC_ROOT = STATIC_DIR
 
 MEDIA_URL = '/media/'
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
